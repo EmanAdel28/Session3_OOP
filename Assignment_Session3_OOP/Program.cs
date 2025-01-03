c941f3a5913c2be927d56be187fc1bc87f80f92b
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-03 19:58:31 +0200

    Part02 Q2

--- Assignment_Session3_OOP/Program.cs
+++ Assignment_Session3_OOP/Program.cs
@@ -8,25 +8,64 @@
             //1 Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects
             //and populate it with data. Then, write a C# program to display the details of all the persons in the array.
 
+            //Person[] person = new Person[3];
+
+            //person[0] = new Person("Eman" , 23);
+            //person[1] = new Person("Amira" , 23);
+            //person[2] = new Person("Mohamed" , 34);
+
+
+            //Console.WriteLine("Data of Persons : ");
+            //try
+            //{
+
+            //    for (int i = 0; i < person?.Length; i++)
+            //    {
+            //        Console.WriteLine(person[i]);
+            //    }
+            //}catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
+            #endregion
+
+            #region Part02 Q2
+            //2 Create a struct called "Person" with properties "Name" and "Age".
+            //Write a C# program that takes details of 3 persons as input from
+            //the user and displays the name and age of the oldest person.
+
             Person[] person = new Person[3];
+            bool Flags;
+            int Age;
+            Console.WriteLine("Enter data of user : ");
 
-            person[0] = new Person("Eman" , 23);
-            person[1] = new Person("Amira" , 23);
-            person[2] = new Person("Mohamed" , 34);
-
-
-            Console.WriteLine("Data of Persons : ");
-            try
+            for(int i = 0; i < person?.Length; i++)
             {
+                    Console.Write($"Enter Name Of Person {i+1} : ");
+                person[i].Name= Console.ReadLine();
 
-                for (int i = 0; i < person?.Length; i++)
+                do
                 {
-                    Console.WriteLine(person[i]);
-                }
-            }catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                    Console.Write($"Enter Age Of Person {i+1} : ");
+                    Flags = int.TryParse(Console.ReadLine(), out  Age);
+
+                } while (!Flags || Age < 0);
+
+                person[i].Age = Age;
             }
+            Console.Clear();
+            Console.WriteLine("Data Of The Oldest Person. ");
+
+            Person oldestPerson = person[0];
+            for (int i = 1; i < person.Length; i++)
+            {
+                if (person[i].Age > oldestPerson.Age)
+                {
+                    oldestPerson = person[i];
+                }
+            }
+            Console.WriteLine(oldestPerson);
+
             #endregion
         }
     }
