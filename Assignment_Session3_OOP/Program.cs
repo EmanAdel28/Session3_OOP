84f526e1cddaa175e9606d27467d04c54765d9c4
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-03 20:06:51 +0200

    Part03

--- Assignment_Session3_OOP/Program.cs
+++ Assignment_Session3_OOP/Program.cs
@@ -34,38 +34,89 @@
             //Write a C# program that takes details of 3 persons as input from
             //the user and displays the name and age of the oldest person.
 
-            Person[] person = new Person[3];
-            bool Flags;
-            int Age;
-            Console.WriteLine("Enter data of user : ");
+            //Person[] person = new Person[3];
+            //bool Flags;
+            //int Age;
+            //Console.WriteLine("Enter data of user : ");
 
-            for(int i = 0; i < person?.Length; i++)
+            //for(int i = 0; i < person?.Length; i++)
+            //{
+            //        Console.Write($"Enter Name Of Person {i+1} : ");
+            //    person[i].Name= Console.ReadLine();
+
+            //    do
+            //    {
+            //        Console.Write($"Enter Age Of Person {i+1} : ");
+            //        Flags = int.TryParse(Console.ReadLine(), out  Age);
+
+            //    } while (!Flags || Age < 0);
+
+            //    person[i].Age = Age;
+            //}
+            //Console.Clear();
+            //Console.WriteLine("Data Of The Oldest Person. ");
+
+            //Person oldestPerson = person[0];
+            //for (int i = 1; i < person.Length; i++)
+            //{
+            //    if (person[i].Age > oldestPerson.Age)
+            //    {
+            //        oldestPerson = person[i];
+            //    }
+            //}
+            //Console.WriteLine(oldestPerson);
+
+            #endregion
+
+            #region Part03 
+            HiringDate hireDate1 = null;
+            HiringDate hireDate2 = null;
+            HiringDate hireDate3 = null;
+
+            try
             {
-                    Console.Write($"Enter Name Of Person {i+1} : ");
-                person[i].Name= Console.ReadLine();
-
-                do
-                {
-                    Console.Write($"Enter Age Of Person {i+1} : ");
-                    Flags = int.TryParse(Console.ReadLine(), out  Age);
-
-                } while (!Flags || Age < 0);
-
-                person[i].Age = Age;
+                hireDate1 = new HiringDate(2, 1, 2024); // Invalid month
+                hireDate2 = new HiringDate(20, 3, 2023); // Invalid month
+                hireDate3 = new HiringDate(16, 5, 2022); // Invalid month
             }
-            Console.Clear();
-            Console.WriteLine("Data Of The Oldest Person. ");
-
-            Person oldestPerson = person[0];
-            for (int i = 1; i < person.Length; i++)
+            catch (ArgumentOutOfRangeException ex)
             {
-                if (person[i].Age > oldestPerson.Age)
-                {
-                    oldestPerson = person[i];
-                }
+                Console.WriteLine(ex.Message); // Will print: "Month must be between 1 and 12."
             }
-            Console.WriteLine(oldestPerson);
 
+            Employee[] employee = new Employee[3];
+
+            employee[0] = new Employee(1, "Eman", SecurityLevel.Developer, 20000, hireDate1, Gender.F);
+            employee[1] = new Employee(2, "Amira", SecurityLevel.Developer, 30000, hireDate2, Gender.F);
+            employee[2] = new Employee(3, "Nohammed", SecurityLevel.Developer, 40000, hireDate3, Gender.M);
+
+            //for(int i = 0; i < employee.Length; i++)
+            //{
+            //    Console.WriteLine(employee[i]);
+
+            //}
+
+            Console.WriteLine("Before Sorting : \n");
+            foreach (var emp in employee)
+            {
+                Console.WriteLine(emp);
+            }
+
+            // Sorting based on HireDate and counting boxing/unboxing
+            int boxingCount = 0;
+            Array.Sort(employee, (x, y) =>
+            {
+                boxingCount++; // Boxing during comparison
+                return x.HireDate.CompareTo(y.HireDate);
+            });
+
+            Console.WriteLine("\nAfter Sorting : \n");
+            foreach (var emp in employee)
+            {
+                Console.WriteLine(emp);
+            }
+
+            Console.WriteLine($"\nNumber of times boxing occurred during sorting: {boxingCount}");
             #endregion
         }
     }
